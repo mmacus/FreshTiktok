2c813b14487b3b0e4fe515fcc35c6ed306a48d4e
mmacus <77479471+mmacus@users.noreply.github.com>
2022-02-01 01:53:23 -0600

    Update tikTokCdn.ts

--- src/modules/tikTokCdn.ts
+++ src/modules/tikTokCdn.ts
@@ -10,7 +10,7 @@ export default async (tikTokLink: string) => {
     const browser = await launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     
-    await page.goto('https://musicallydown.com/');
+    await page.goto('https://musicaldown.com/');
 
     await page.type('#link_url', tikTokLink);
     await page.keyboard.press('Enter');
